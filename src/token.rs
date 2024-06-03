2b56802c45918f4ddc01dcf9aeb1b0e405ce40d2
Lysender <lysender@darkstar-win>
2024-06-03 10:11:17 +0800

    fix: captcha token generator

--- src/token.rs
+++ src/token.rs
@@ -5,7 +5,8 @@ use crate::error::Result;
 pub fn create_captcha_token(secret: &str) -> Result<String> {
     let key = HS256Key::from_bytes(secret.as_bytes());
     let claims = Claims::create(Duration::from_hours(1)).with_subject("x-client-login");
-    key.authenticate(claims)
+    let token = key.authenticate(claims)?;
+    Ok(format!("x-client-login:{}", token))
 }
 
 #[cfg(test)]
